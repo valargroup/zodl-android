9b9c9e9fb3d6a80964d0dbd4f5f718077aa1aaa1
NoOp <dominik@zodl.com>
2026-04-16 22:29:38 +0200

    MOB-1100: Wrapped keystone SDK into interface and added addition handling

--- ui-lib/src/main/java/co/electriccoin/zcash/ui/common/usecase/ParseKeystoneSignInRequestUseCase.kt
+++ ui-lib/src/main/java/co/electriccoin/zcash/ui/common/usecase/ParseKeystoneSignInRequestUseCase.kt
@@ -1,14 +1,16 @@
 package co.electriccoin.zcash.ui.common.usecase
 
 import co.electriccoin.zcash.ui.NavigationRouter
+import co.electriccoin.zcash.ui.common.provider.KeystoneSDKProvider
 import co.electriccoin.zcash.ui.screen.selectkeystoneaccount.SelectKeystoneAccount
 import com.sparrowwallet.hummingbird.UR
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class ParseKeystoneSignInRequestUseCase(
-    private val navigationRouter: NavigationRouter
-) : BaseKeystoneScanner() {
+    private val navigationRouter: NavigationRouter,
+    keystoneSDKProvider: KeystoneSDKProvider,
+) : BaseKeystoneScanner(keystoneSDKProvider) {
     override suspend fun onSuccess(ur: UR) {
         parseOrThrow(ur)
         navigationRouter.replace(SelectKeystoneAccount(ur.toString()))
@@ -19,7 +21,7 @@ class ParseKeystoneSignInRequestUseCase(
     private suspend fun parseOrThrow(ur: UR) =
         withContext(Dispatchers.Default) {
             try {
-                keystoneSDK.parseZcashAccounts(ur)
+                keystoneSDKProvider.parseZcashAccounts(ur)
             } catch (e: Exception) {
                 throw InvalidKeystoneSignInQRException(e)
             }
